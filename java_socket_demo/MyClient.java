ab2e3d063fa98d66203beda7551ca64b9645a1ca
johnwang <monkeygoessnap@gmail.com>
2022-02-20 00:42:57 +0800

    changed to multi-client socket implementation

--- java_socket_demo/MyClient.java
+++ java_socket_demo/MyClient.java
@@ -1,21 +1,52 @@
-import java.net.*;  
-import java.io.*;  
-class MyClient{  
-public static void main(String args[])throws Exception{  
-Socket s=new Socket("localhost",3333);  
-DataInputStream din=new DataInputStream(s.getInputStream());  
-DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
   
-String str="",str2="";  
-while(!str.equals("stop")){  
-str=br.readLine();
-dout.writeUTF(str);  
-dout.flush();  
-str2=din.readUTF();  
-System.out.println("Server says: "+str2);  
-}  
-  
-dout.close();  
-s.close();  
-}}  
+// Client class
+public class MyClient 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        try
+        {
+            Scanner scn = new Scanner(System.in);
+              
+            // getting localhost ip
+            InetAddress ip = InetAddress.getByName("localhost");
+      
+            // establish the connection with server port 3333
+            Socket s = new Socket(ip, 3333);
+      
+            // obtaining input and out streams
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+      
+            // the following loop performs the exchange of
+            // information between client and client handler
+            while (true) 
+            {
+                
+                System.out.println(dis.readUTF());
+                String tosend = scn.nextLine();
+                dos.writeUTF(tosend);
+                  
+                // If client sends exit,close this connection 
+                // and then break from the while loop
+                if(tosend.equals("Exit"))
+                {
+                    System.out.println("Closing this connection : " + s);
+                    s.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+            }
+              
+            // closing resources
+            scn.close();
+            dis.close();
+            dos.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
--- java_socket_demo/MyServer.java
+++ java_socket_demo/MyServer.java
@@ -1,28 +1,132 @@
-import java.net.*;  
-import java.io.*;  
-class MyServer{  
-public static void main(String args[])throws Exception{  
-ServerSocket ss=new ServerSocket(3333);  
-Socket s=ss.accept();  
-DataInputStream din=new DataInputStream(s.getInputStream());  
-DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+
+// Java implementation of  Server side
+// It contains two classes : Server and ClientHandler
+// Save file as Server.java
   
-String str="",str2="";  
-while(!str.equals("stop")){  
-str=din.readUTF();  
-System.out.println(str+"abc");
-System.out.println("client says: "+str);
-if (str.equals("ping")) {
-    str2 = "pong";
-} else {
-    str2 = "pls type ping";
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.net.*;
+  
+// Server class
+public class MyServer 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        // server is listening on port 5056
+        ServerSocket ss = new ServerSocket(3333);
+          
+        // running infinite loop for getting
+        // client request
+        while (true) 
+        {
+            Socket s = null;
+              
+            try 
+            {
+                // socket object to receive incoming client requests
+                s = ss.accept();
+                  
+                System.out.println("A new client is connected : " + s);
+                  
+                // obtaining input and out streams
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                  
+                System.out.println("Assigning new thread for this client");
+  
+                // create a new thread object
+                Thread t = new ClientHandler(s, dis, dos);
+  
+                // Invoking the start() method
+                t.start();
+                  
+            }
+            catch (Exception e){
+                s.close();
+                e.printStackTrace();
+            }
+        }
+    }
 }
-//str2=br.readLine();  
-dout.writeUTF(str2);  
-dout.flush();  
-}  
-din.close();  
-s.close();  
-ss.close();  
-}}  
+  
+// ClientHandler class
+class ClientHandler extends Thread 
+{
+    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd");
+    DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    final Socket s;
+      
+  
+    // Constructor
+    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos) 
+    {
+        this.s = s;
+        this.dis = dis;
+        this.dos = dos;
+    }
+  
+    @Override
+    public void run() 
+    {
+        String received;
+        String toreturn;
+        while (true) 
+        {
+            try {
+  
+                // Ask user what he wants
+                dos.writeUTF("What do you want?[Date | Time]..\n"+
+                            "Type Exit to terminate connection.");
+                  
+                // receive the answer from client
+                received = dis.readUTF();
+                  
+                if(received.equals("Exit"))
+                { 
+                    System.out.println("Client " + this.s + " sends exit...");
+                    System.out.println("Closing this connection.");
+                    this.s.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+                  
+                // creating Date object
+                Date date = new Date();
+                  
+                // write on output stream based on the
+                // answer from the client
+                switch (received) {
+                  
+                    case "Date" :
+                        toreturn = fordate.format(date);
+                        dos.writeUTF(toreturn);
+                        break;
+                          
+                    case "Time" :
+                        toreturn = fortime.format(date);
+                        dos.writeUTF(toreturn);
+                        break;
+                          
+                    default:
+                        dos.writeUTF("Invalid input");
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+          
+        try
+        {
+            // closing resources
+            this.dis.close();
+            this.dos.close();
+              
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
